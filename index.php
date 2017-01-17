b6ab7bfd7fc325c0261d5ca3c261a5aa52233999
Feng CHEN <fchensecurity@163.com>
2017-01-17 21:15:33 +0800

    navbar change and index.php added

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Feng CHEN, IPEL, HKUST, Power Management IC Designer</title>
+<meta http-equiv="pragma" content="no-cache"> 
+<meta http-equiv="cache-control" content="no-cache, must-revalidate"> 
+<meta http-equiv="expires" content="0"> 
+<meta http-equiv="refresh" content="0;URL=mypage" /> 
+
+</head>
+</html>
