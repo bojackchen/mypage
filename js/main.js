1b8dcc9415d1dd903f3dea982520155cd83591e1
Feng CHEN <fchensecurity@163.com>
2017-09-02 15:30:27 +0800

    First breakdown

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
 $(document).on("click", ".nav a", function(item) {
-    // Collapse navbar automatically after click on an item
-    $("#myNavbar").collapse("hide");
+  // Collapse navbar automatically after click on an item
+  $("#myNavbar").collapse("hide");
 });
 
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    // Lightbox activation
-    event.preventDefault();
-    $(this).ekkoLightbox();
+$(document).on("click", "[data-toggle='lightbox']", function(event) {
+  // Lightbox activation
+  event.preventDefault();
+  $(this).ekkoLightbox();
 });
+
+$(document).on("click", ".navbar a, #touch a, #navbrand, footer a[href='#myPage']", function(event) {
+  // Add smooth scrolling to all links in navbar + footer link
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $("html, body").animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+      window.location.hash = hash;
+    });
+  }
+})
 
 $(document).ready(function() {
-    // Add smooth scrolling to all links in navbar + footer link
-    $(".navbar a, #touch a, #navbrand, footer a[href='#myPage']").on("click", function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $("html, body").animate({
-                scrollTop: $(hash).offset().top
-            }, 900, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
-
-    // Initialize tooltip
-    $("[data-toggle='tooltip']").tooltip(); 
+  // Initialize tooltip
+  $("[data-toggle='tooltip']").tooltip(); 
 })
 
 // Add googleMap display
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("googleMap"), {
-        center: {lat: 22.334936, lng: 114.263531},
-        zoom: 16,
-        scrollwheel: false,
-        draggable: true,
-    });
+  var map = new google.maps.Map(document.getElementById("googleMap"), {
+    center: {lat: 22.334936, lng: 114.263531},
+    zoom: 16,
+    scrollwheel: false,
+    draggable: true,
+  });
 
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(22.336346, 114.265460),
-    });
-    marker.setMap(map);
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(22.336346, 114.265460),
+  });
+  marker.setMap(map);
 }
 google.maps.event.addDomListener(window, "load", initMap);
 
 // Slide animation of logos
 $(window).scroll(function() {
-    $(".slideanim").each(function() {
-        var pos = $(this).offset().top;
-        var winTop = $(window).scrollTop();
-        if (pos < winTop + 700) {
-            $(this).addClass("slide");
-        }
-    });
+  $(".slideanim").each(function() {
+    var pos = $(this).offset().top;
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 700) {
+      $(this).addClass("slide");
+    }
+  });
 });
 
 function searchJump() {
-    window.open("http://www.google.com/#q=" + document.getElementById("searchtext").value.split(" ").join("+"),
-        "_blank");//.focus();
+  window.open("http://www.google.com/#q=" + document.getElementById("searchtext").value.split(" ").join("+"),
+    "_blank");//.focus();
 }
